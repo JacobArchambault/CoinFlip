030dde8524db8c164d0e4c12d4a248ccdcc137c6
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-24 12:50:44 -0400

    Cleanup

--- src/main/java/com/jacobarchambault/coinflip/App.java
+++ src/main/java/com/jacobarchambault/coinflip/App.java
@@ -8,7 +8,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -17,29 +16,29 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
+	public static void main(final String[] args) {
+		Application.launch();
+	}
+
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(final Stage primaryStage) {
 		// Create the ImageView control.
-		Coin coinImage = new Coin(new Image("file:Heads1.png"), new Image("file:Tails1.png"), new Random());
+		final var coinImage = new Coin(new Image("file:Heads1.png"), new Image("file:Tails1.png"), new Random());
 		coinImage.setFitWidth(150);
 		coinImage.setFitHeight(150);
 		coinImage.setPreserveRatio(true);
 		// Create the tossButton control.
-		Button tossButton = new Button("Toss");
+		final var tossButton = new Button("Toss");
 		// Register the event handler.
 		tossButton.setOnAction(e -> coinImage.flip());
 		// Put everything into a VBox
-		VBox mainVBox = new VBox(10, coinImage, tossButton);
+		final var mainVBox = new VBox(10, coinImage, tossButton);
 		mainVBox.setAlignment(Pos.CENTER);
 		mainVBox.setPadding(new Insets(10));
 		// Add the main VBox to a scene.
 		// Set the scene to the stage and display it.
 		primaryStage.setScene(new Scene(mainVBox));
 		primaryStage.show();
-	}
-
-	public static void main(String[] args) {
-		launch();
 	}
 
 }
